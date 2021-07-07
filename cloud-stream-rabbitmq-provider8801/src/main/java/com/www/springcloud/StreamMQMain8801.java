fcd6384cb046d079d04396c3be566587964eb983
wangweiwei_P28 <wangweiwei.sh@chinaccs.cn>
2021-07-07 14:22:38 +0800

    提交：pom配置文件

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/www/springcloud/StreamMQMain8801.java
@@ -0,0 +1,17 @@
+package com.www.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author www
+ * @version 1.0
+ * @create 2021/7/2 10:14
+ */
+@SpringBootApplication
+public class StreamMQMain8801 {
+     public static void main(String[] args) {
+           SpringApplication.run(StreamMQMain8801.class, args);
+      }
+
+}
